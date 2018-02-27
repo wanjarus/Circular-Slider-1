de1c5cc4ee91f25ddf2e8dca89219271f002e06d
Slavko Djogic <djogicslavko@gmail.com>
2018-02-27 18:47:25 +0100

    Responsive fix

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
+var id = 1;
+
 (function() {
-  var container = document.getElementById("container");
+  var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("class", "progress");
+  svg.setAttribute("width", 500);
+  svg.setAttribute("height", 500);
+  svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  svg.setAttribute("viewBox", `0 0 ${500} ${500}`);
 
   slider1 = new circularSlider({
-    container: container,
+    container: svg,
     color: "#663A69",
-    range: [0, 1000],
+    range: [100, 1000],
     step: 1,
     radius: 250,
     text: "Transportation"
@@ -12,7 +19,7 @@
   slider1.handleInput();
 
   slider2 = new circularSlider({
-    container: container,
+    container: svg,
     color: "#0075B0",
     range: [0, 800],
     step: 1,
@@ -22,7 +29,7 @@
   slider2.handleInput();
 
   slider3 = new circularSlider({
-    container: container,
+    container: svg,
     color: "#009D33",
     range: [0, 2350],
     step: 1,
@@ -32,7 +39,7 @@
   slider3.handleInput();
 
   slider4 = new circularSlider({
-    container: container,
+    container: svg,
     color: "#FF7D36",
     range: [0, 1567],
     step: 1,
@@ -42,7 +49,7 @@
   slider4.handleInput();
 
   slider5 = new circularSlider({
-    container: container,
+    container: svg,
     color: "#FF394B",
     range: [0, 500],
     step: 1,
@@ -51,32 +58,23 @@
   });
   slider5.handleInput();
 
+  document.querySelector(".sliderContainer").appendChild(svg);
 })();
 
 function circularSlider(options) {
   var mouseDown = false;
-  var CIRCUMFERENCE = 2 * Math.PI * (options.radius - 6);
 
-  var sliderContainer = document.createElement("div");
-  sliderContainer.setAttribute("class", "sliderContainer");
-
-  var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.setAttribute("class", "progress");
-  svg.setAttribute("width", options.radius * 2);
-  svg.setAttribute("height", options.radius * 2);
-  svg.setAttribute('xmlns', "http://www.w3.org/2000/svg");
-  svg.setAttribute(
-    "viewBox",
-    `0 0 ${options.radius * 2} ${options.radius * 2}`
-  );
+  var group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  group.setAttribute("id", id);
+  id++;
 
   var progressMeter = document.createElementNS(
     "http://www.w3.org/2000/svg",
     "circle"
   );
   progressMeter.setAttribute("class", "progress__meter");
-  progressMeter.setAttribute("cx", options.radius);
-  progressMeter.setAttribute("cy", options.radius);
+  progressMeter.setAttribute("cx", options.container.getAttribute("width") / 2);
+  progressMeter.setAttribute("cy", options.container.getAttribute("width") / 2);
   progressMeter.setAttribute("r", options.radius - 6);
   progressMeter.setAttribute("stroke-width", 12);
 
@@ -85,14 +83,14 @@ function circularSlider(options) {
     "circle"
   );
   progressValue.setAttribute("class", "progress__value");
-  progressValue.setAttribute("cx", options.radius);
-  progressValue.setAttribute("cy", options.radius);
+  progressValue.setAttribute("cx", options.container.getAttribute("width") / 2);
+  progressValue.setAttribute("cy", options.container.getAttribute("width") / 2);
   progressValue.setAttribute("r", options.radius - 6);
   progressValue.setAttribute("stroke-width", 12);
   progressValue.style.stroke = options.color;
 
-  var dial = document.createElement('div');
-  dial.setAttribute('class', 'dial');
+  var dial = document.createElement("div");
+  dial.setAttribute("class", "dial");
 
   var input = document.createElement("input");
   input.setAttribute("type", "range");
@@ -101,65 +99,70 @@ function circularSlider(options) {
   input.setAttribute("min", options.range[0]);
   input.setAttribute("max", options.range[1]);
   input.setAttribute("step", options.step);
-  input.setAttribute("value", "0");
-  input.addEventListener("input", function(event) {
-    this.progress(event.target.valueAsNumber);
+  input.setAttribute("value", options.range[0]);
+  input.addEventListener("input", e => {
+    this.progress(input.valueAsNumber);
   });
 
   var pricing = document.createElement("span");
-  pricing.setAttribute('class', 'pricing');
+  pricing.setAttribute("class", "pricing");
   pricing.textContent = "$" + options.range[0];
 
   var box = document.createElement("span");
-  box.setAttribute('class', 'box');
+  box.setAttribute("class", "box");
   box.setAttribute("style", "background-color: " + options.color);
 
   var text = document.createElement("span");
-  text.setAttribute('class', 'text');
+  text.setAttribute("class", "text");
   text.textContent = options.text;
 
   var div = document.createElement("div");
-  div.setAttribute('class', 'textContainer');
+  div.setAttribute("class", "textContainer");
 
-  svg.appendChild(progressMeter);
-  svg.appendChild(progressValue);
-
-  sliderContainer.appendChild(svg);
-  sliderContainer.appendChild(dial);
-  sliderContainer.appendChild(input);
+  group.appendChild(progressMeter);
+  group.appendChild(progressValue);
+  // group.appendChild(dial);
+  // group.appendChild(input);
+  options.container.appendChild(group);
 
   div.appendChild(pricing);
   div.appendChild(box);
   div.appendChild(text);
-  document.querySelector('.price').appendChild(div);
-
-  options.container.appendChild(sliderContainer);
+  document.querySelector(".price").appendChild(div);
 
   this.handleInput = () => {
-    sliderContainer.addEventListener("mouseup", (e) => {
-      e.path[1].style.zIndex = '0';
+    group.addEventListener("mouseup", e => {
+      // e.path[1].style.zIndex = '0';
+      group.style.zIndex = "0";
       mouseDown = false;
     });
-    sliderContainer.addEventListener("touchend", (e) => {
-      e.path[1].style.zIndex = '0';
+    group.addEventListener("touchend", e => {
+      // e.path[1].style.zIndex = '0';
+      group.style.zIndex = "0";
       mouseDown = false;
     });
-    sliderContainer.addEventListener("mousedown", (e) => {
-      e.path[1].style.zIndex = '123';
+    group.addEventListener("mousedown", e => {
+      // e.path[1].style.zIndex = '123';
+      group.style.zIndex = "123";
       mouseDown = true;
     });
-    sliderContainer.addEventListener("touchstart", (e) => {
-      e.path[1].style.zIndex = '123';
+    group.addEventListener("touchstart", e => {
+      // e.path[1].style.zIndex = '123';
+      group.style.zIndex = "123";
       mouseDown = true;
     });
     progressMeter.addEventListener("click", this.update);
     progressValue.addEventListener("click", this.update);
     document.addEventListener("mousemove", this.update);
-    document.addEventListener("touchmove", this.update, {passive: false});
+    document.addEventListener("mouseup", () => {
+      group.style.zIndex = "0";
+      mouseDown = false;
+    });
+    document.addEventListener("touchmove", this.update, { passive: false });
   };
 
-  this.update = (e) => {
-    if(e.type != 'click') {
+  this.update = e => {
+    if (e.type != "click") {
       if (!mouseDown || options.range[1] == 0) return;
       this.move(e);
     } else {
@@ -167,49 +170,72 @@ function circularSlider(options) {
     }
   };
 
-  this.move = (e) => {
-    e.path[1].style.zIndex = '123';
+  this.move = e => {
+    // e.path[1].style.zIndex = '123';
+    // sliderContainer.style.zIndex = '123';
 
     // console.log('Event: ' + e.type);
     var position;
-    if(e.type == 'mouseup' || e.type == 'mousedown' || e.type == 'mousemove' || e.type == 'click') {
+    if (
+      e.type == "mouseup" ||
+      e.type == "mousedown" ||
+      e.type == "mousemove" ||
+      e.type == "click"
+    ) {
       position = { x: e.pageX, y: e.pageY };
-    } else if(e.type == 'touchend' || e.type == 'touchstart' || e.type == 'touchmove') {
+    } else if (
+      e.type == "touchend" ||
+      e.type == "touchstart" ||
+      e.type == "touchmove"
+    ) {
       e.preventDefault();
       position = { x: e.touches[0].clientX, y: e.touches[0].clientY };
     }
 
     var dialRadius = dial.offsetWidth / 2;
     var coords = {
-      x: position.x - sliderContainer.offsetLeft,
-      y: position.y - sliderContainer.offsetTop
+      x: position.x - group.getBoundingClientRect().left,
+      y: position.y - group.getBoundingClientRect().top
     };
     var atan = Math.atan2(coords.x - options.radius, coords.y - options.radius);
     var deg = Math.ceil(-atan / (Math.PI / 180) + 180);
 
-    var x = Math.ceil((options.radius - 5) * Math.sin(deg * Math.PI / 180)) + options.radius + "px";
-    var y = Math.ceil((options.radius - 5) * -Math.cos(deg * Math.PI / 180)) + options.radius + "px";
+    var x =
+      Math.ceil((options.radius - 5) * Math.sin(deg * Math.PI / 180)) +
+      options.radius +
+      "px";
+    var y =
+      Math.ceil((options.radius - 5) * -Math.cos(deg * Math.PI / 180)) +
+      options.radius +
+      "px";
     var points = Math.ceil(deg * options.range[1] / 360);
 
     dial.style.transform = "translate(" + x + "," + y + ")";
 
     // console.log(text.textContent + ": $" + points);
-    pricing.textContent = "$" + points;
-    this.progress(points);
-  }
+    input.value = points;
+    pricing.textContent = "$" + input.value;
+    this.progress(input.value);
+  };
 
-  this.progress = (value) => {
+  this.progress = value => {
     // console.log('Value: ' + value);
+    progressValue.style.strokeDasharray = 2 * Math.PI * (options.radius - 6);
     var progress = value / options.range[1];
-    var dashoffset = CIRCUMFERENCE * (1 - progress);
+    var dashoffset = 2 * Math.PI * (options.radius - 6) * (1 - progress);
     // console.log('dashoffset: ' + dashoffset);
     progressValue.style.strokeDashoffset = dashoffset;
   };
 
-  progressValue.style.strokeDasharray = CIRCUMFERENCE;
   this.progress(input.value);
 
-  var xx = Math.ceil((options.radius - 5) * Math.sin(1 * Math.PI / 180)) + options.radius + "px";
-  var yy = Math.ceil((options.radius - 5) * -Math.cos(1 * Math.PI / 180)) + options.radius + "px";
+  var xx =
+    Math.ceil((options.radius - 5) * Math.sin(1 * Math.PI / 180)) +
+    options.radius +
+    "px";
+  var yy =
+    Math.ceil((options.radius - 5) * -Math.cos(1 * Math.PI / 180)) +
+    options.radius +
+    "px";
   dial.style.transform = "translate(" + xx + "," + yy + ")";
 }
